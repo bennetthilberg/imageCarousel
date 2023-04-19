c28bfed5e4c6db813d26bcaea6df90d9e069e0a5
bennetthilberg <bennett.hilberg@gmail.com>
2023-04-19 10:10:13 -0500

    Finish by adding javascript functionality and fixing CSS

--- js.js
+++ js.js
@@ -1,8 +1,24 @@
 let next = document.querySelector('#next');
 let prev = document.querySelector('#prev');
+let carousel = document.querySelector('#carousel');
+
+let carouselPos = 0;
 
 next.addEventListener('click', () => nextSlide());
+prev.addEventListener('click', () => prevSlide())
 
 function nextSlide(){
-    carousel.style.transform = translateX('50px');
+    if(carouselPos == -1600){
+        carouselPos = 0;
+    }
+    else{carouselPos = carouselPos-800;}
+    carousel.style.transform = `translateX(${carouselPos}px)`;
 }
+function prevSlide(){
+    if(carouselPos == 0){
+        carouselPos = -1600;
+    }
+    else{carouselPos = carouselPos+800;}
+    carousel.style.transform = `translateX(${carouselPos}px)`;
+}
+setInterval(nextSlide, 3000);
